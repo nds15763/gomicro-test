afdcc37bb142692fbb59073660330cd0adcb48f6
nds15763 <425257452@qq.com>
2020-05-21 13:56:20 +0800

    edit:初次调试通过

--- laracom/demo-service/main.go
+++ laracom/demo-service/main.go
@@ -2,78 +2,32 @@ package main
 
 import (
 	"context"
-	"flag"
-	"fmt"
-	"laracom/demo-service/api"
-	pb "laracom/demo-service/proto/demo"
 	"log"
-	"net"
 
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
+	pb "laracom/demo-service/proto/demo"
+
+	"github.com/micro/go-micro/v2"
 )
 
-const (
-	address  = "localhost:9999"
-	grpcPort = ":9999"
-	httpPort = "8000"
-	appName  = "Demo Service"
-)
-
-type DemoService struct {
+type DemoServiceHandler struct {
 }
 
-func (ds *DemoService) SayHello(ctx context.Context, req *pb.DemoRequest) (*pb.DemoResponse, error) {
-	return &pb.DemoResponse{Text: "你好, " + req.Name}, nil
+func (s *DemoServiceHandler) SayHello(ctx context.Context, req *pb.DemoRequest, rsp *pb.DemoResponse) error {
+	rsp.Text = "你好, " + req.Name
+	return nil
+}
+
+func (s *DemoServiceHandler) SayHelloByUserId(ctx context.Context, req *pb.HelloRequest, rsp *pb.DemoResponse) error {
+	return nil
 }
 
 func main() {
-	// 通过-mode参数判断启动哪个模式的代码
-	mode := flag.String("mode", "grpc", "mode:grpc/http/client")
-	flag.Parse()
-	fmt.Println("run mode:", *mode)
-	switch *mode {
-	case "http":
-		{
-			fmt.Printf("Starting %v\n", appName)
-			api.StartWebServer(httpPort)
-		}
-	case "client":
-		{
-			//WithInsecure返回一个DialOption，它在传输过程中不保证安全。除非设置WithInsecure，否则grpc.Dial必须指定安全选项。
-			conn, err := grpc.Dial(address, grpc.WithInsecure())
-			if err != nil {
-				log.Fatalf("连接到 gRPC 服务器失败: %v", err)
-			}
+	// 注册服务名必须和 demo.proto 中的 package 声明一致
+	service := micro.NewService(micro.Name("laracom.demo.service"))
+	service.Init()
 
-			defer conn.Close()
-
-			client := pb.NewDemoServiceClient(conn)
-			req := &pb.DemoRequest{Name: "学院君"}
-			rsp, err := client.SayHello(context.Background(), req)
-			if err != nil {
-				log.Fatalf("调用 gRPC 服务接口失败: %v", err)
-			}
-			log.Printf("%s", rsp.Text)
-		}
-	case "grpc":
-		fallthrough
-	default:
-		{
-			listener, err := net.Listen("tcp", grpcPort)
-			if err != nil {
-				log.Fatalf("监听指定端口失败: %v", err)
-			}
-
-			server := grpc.NewServer()
-			pb.RegisterDemoServiceServer(server, &DemoService{})
-
-			reflection.Register(server)
-
-			if err := server.Serve(listener); err != nil {
-				log.Fatalf("服务启动失败: %v", err)
-			}
-		}
+	pb.RegisterDemoServiceHandler(service.Server(), &DemoServiceHandler{})
+	if err := service.Run(); err != nil {
+		log.Fatalf("服务启动失败: %v", err)
 	}
-
 }
--- laracom/go.mod
+++ laracom/go.mod
@@ -2,9 +2,28 @@ module laracom
 
 go 1.14
 
+replace google.golang.org/grpc v1.29.1 => google.golang.org/grpc v1.26.0
+
 require (
+	github.com/coreos/etcd v3.3.21+incompatible // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/gogo/protobuf v1.3.1 // indirect
 	github.com/golang/protobuf v1.4.2
 	github.com/gorilla/mux v1.7.4
-	golang.org/x/net v0.0.0-20190311183353-d8887717615a
-	google.golang.org/grpc v1.29.1
+	github.com/micro/go-micro v1.18.0
+	github.com/micro/go-micro/v2 v2.7.0
+	github.com/miekg/dns v1.1.29 // indirect
+	github.com/nats-io/nats.go v1.10.0 // indirect
+	github.com/prometheus/client_golang v1.2.1 // indirect
+	github.com/sirupsen/logrus v1.5.0 // indirect
+	go.uber.org/zap v1.15.0 // indirect
+	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37 // indirect
+	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
+	golang.org/x/net v0.0.0-20200520004742-59133d7f0dd7
+	golang.org/x/sys v0.0.0-20200519105757-fe76b779f299 // indirect
+	golang.org/x/tools v0.0.0-20200519205726-57a9e4404bf7 // indirect
+	google.golang.org/genproto v0.0.0-20200519141106-08726f379972 // indirect
+	google.golang.org/grpc v1.29.1 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+	honnef.co/go/tools v0.0.1-2020.1.4 // indirect
 )
